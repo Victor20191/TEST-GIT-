4ebc2cb63987575bcecef840ddeb819e44ba6d8f
Spaguetto <48161646+Spaguetto@users.noreply.github.com>
2024-01-29 18:17:26 -0500

    Interactua con estos cambios!

---
+++ app.js
@@ -0,0 +1,5 @@
+//hey! es un bonito dia para hacer tu primer Hola mundo en JS!
+//Escribe un console.log o un alert como el del ejemplo>
+
+console.log("Ten un lindo dia!");
+alert('y que sea muy especial!');
